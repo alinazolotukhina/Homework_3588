7fb9e566dff14428ae4b83bb27ac30c433d19514
«Alina» <laim31@gmail.com>
2022-12-30 15:07:16 +0300

    Sem9Task68

---
+++ Sem9Task68/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+using System.Numerics;
+ulong Akk(ulong n, ulong m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Akk(n - 1, 1);
+    else
+        return Akk(n - 1, Akk(n, m - 1));
+}
+ ulong ReadData(string line)
+{
+    Console.Write(line);
+    ulong number = ulong.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(ulong number )
+{
+    Console.WriteLine(number);
+}
+ulong m = ReadData("Введите первое число: ");
+ulong n = ReadData("Введите второе число: ");
+ulong number = Akk(n,m);
+PrintResult( number );
